e1686bd4c200123ed91d7ac9bf43087ae9994d75
dimitar-seraffimov <91837728+dimitar-seraffimov@users.noreply.github.com>
2026-06-01 17:12:13 +0100

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,16 @@
+from collections import Counter, defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+        '''
+        if len(s) != len(t):
+            return False
+        if s == t:
+            return True
+        
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
+        '''
